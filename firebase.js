faffcd80919fd37744e14e76e316e03a331911d7
lihon <gelf.learing@gmail.com>
2024-08-09 23:53:20 +0300

    updating my structure

--- firebase.js
+++ firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,5 +14,6 @@ const firebaseConfig = {
   appId: "1:72068664025:web:f62df54bb160a2aaa18cc8"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
