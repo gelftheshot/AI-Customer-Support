34ad2abd5bc80e71325200359a180c17f5b36444
lihon <gelf.learing@gmail.com>
2024-08-09 23:38:15 +0300

    staring the setup of firebase

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "ai-chat-assistant-ac0b4.firebaseapp.com",
+  projectId: "ai-chat-assistant-ac0b4",
+  storageBucket: "ai-chat-assistant-ac0b4.appspot.com",
+  messagingSenderId: "72068664025",
+  appId: "1:72068664025:web:f62df54bb160a2aaa18cc8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
